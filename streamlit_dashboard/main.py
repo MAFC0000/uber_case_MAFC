f4a828e3b6ce7d69d37f1b431b4e57237ab33551
Miguel Angel Fernandez Castresana <mikejr_2000@hotmail.com>
2025-11-18 21:34:10 -0600

    File modified

--- streamlit_dashboard/main.py
+++ streamlit_dashboard/main.py
@@ -82,96 +82,96 @@ st.sidebar.markdown(f"{page} filters")
 if page=='Historical performance':
     st.subheader("Historical performance")
     
-    # # Filters selection
-    # des_territory=st.sidebar.multiselect('Territory',['South East', 'Central', 'Long Tail - Region', 'West', 'North'])
-    # des_courier_flow=st.sidebar.multiselect('Courier Flow',['Motorbike', 'UberEats', 'Logistics', 'Fleet', 'Onboarder', 'SUV', 'UberX'])
-    # des_geo_archetype=st.sidebar.multiselect('Geography archetype',['Build experience', 'Drive momentum', 'Play offense', 'Defend CP', 'Unlaunched', 'Unspecified'])
-    # des_merchant_surface=st.sidebar.multiselect('Merchant Surface',['Other', 'POS', 'Tablet', 'Unspecified', 'Web/Mobile'])
-    # des_initial_date, des_final_date = st.sidebar.date_input('Eater request date range',value=(date(2025, 3, 1), date(2025, 4, 27)), min_value=(date(2025, 3, 1)), max_value=(date(2025, 4, 30)))
+    # Filters selection
+    des_territory=st.sidebar.multiselect('Territory',['South East', 'Central', 'Long Tail - Region', 'West', 'North'])
+    des_courier_flow=st.sidebar.multiselect('Courier Flow',['Motorbike', 'UberEats', 'Logistics', 'Fleet', 'Onboarder', 'SUV', 'UberX'])
+    des_geo_archetype=st.sidebar.multiselect('Geography archetype',['Build experience', 'Drive momentum', 'Play offense', 'Defend CP', 'Unlaunched', 'Unspecified'])
+    des_merchant_surface=st.sidebar.multiselect('Merchant Surface',['Other', 'POS', 'Tablet', 'Unspecified', 'Web/Mobile'])
+    des_initial_date, des_final_date = st.sidebar.date_input('Eater request date range',value=(date(2025, 3, 1), date(2025, 4, 27)), min_value=(date(2025, 3, 1)), max_value=(date(2025, 4, 30)))
     
-    # # Data loading and filtering according to user input
-    # df=load_data()
-    # try:
-    #     if des_territory!=[]:
-    #         df=df[df['territory'].isin(des_territory)]
-    #     if des_courier_flow!=[]:
-    #         df=df[df['courier_flow'].isin(des_courier_flow)]
-    #     if des_geo_archetype!=[]:
-    #         df=df[df['geo_archetype'].isin(des_geo_archetype)]
-    #     if des_merchant_surface!=[]:
-    #         df=df[df['merchant_surface'].isin(des_merchant_surface)]
-    #     df=df[(df['eater_request_timestamp_local'].dt.date >=pd.to_datetime(des_initial_date).date()) & (df['eater_request_timestamp_local'].dt.date <=pd.to_datetime(des_final_date).date())]
-    # except Exception as e:
-    #     st.error(f"An error occurred while filtering the data: {e}\n Please check your filter selections.")
+    # Data loading and filtering according to user input
+    df=load_data()
+    try:
+        if des_territory!=[]:
+            df=df[df['territory'].isin(des_territory)]
+        if des_courier_flow!=[]:
+            df=df[df['courier_flow'].isin(des_courier_flow)]
+        if des_geo_archetype!=[]:
+            df=df[df['geo_archetype'].isin(des_geo_archetype)]
+        if des_merchant_surface!=[]:
+            df=df[df['merchant_surface'].isin(des_merchant_surface)]
+        df=df[(df['eater_request_timestamp_local'].dt.date >=pd.to_datetime(des_initial_date).date()) & (df['eater_request_timestamp_local'].dt.date <=pd.to_datetime(des_final_date).date())]
+    except Exception as e:
+        st.error(f"An error occurred while filtering the data: {e}\n Please check your filter selections.")
     
-    # # Initial trend graph
-    # st.markdown('The actual trend is:')
-    # try:
-    #     graph=des_initial_graph_prediction_function(df[['delivery_trip_uuid', 'eater_request_timestamp_local', 'ATD']])
-    #     st.pyplot(graph)
-    # except Exception as e:
-    #     graph=des_initial_graph_function(df[['delivery_trip_uuid', 'eater_request_timestamp_local', 'ATD']])
-    #     st.pyplot(graph)
+    # Initial trend graph
+    st.markdown('The actual trend is:')
+    try:
+        graph=des_initial_graph_prediction_function(df[['delivery_trip_uuid', 'eater_request_timestamp_local', 'ATD']])
+        st.pyplot(graph)
+    except Exception as e:
+        graph=des_initial_graph_function(df[['delivery_trip_uuid', 'eater_request_timestamp_local', 'ATD']])
+        st.pyplot(graph)
     
-    # # KPIs calculation and display
-    # des_kpi_total = des_kpi_total_function(df['delivery_trip_uuid'])
-    # des_kpi_atd = des_kpi_atd_function(df['ATD'])
-    # des_kpi_velocity= des_kpi_velocity_function(df['pickup_distance'], df['dropoff_distance'], df['ATD'])
+    # KPIs calculation and display
+    des_kpi_total = des_kpi_total_function(df['delivery_trip_uuid'])
+    des_kpi_atd = des_kpi_atd_function(df['ATD'])
+    des_kpi_velocity= des_kpi_velocity_function(df['pickup_distance'], df['dropoff_distance'], df['ATD'])
 
-    # kpi_1, kpi_2, kpi_3 = st.columns(3) 
+    kpi_1, kpi_2, kpi_3 = st.columns(3) 
 
-    # with kpi_1:
-    #     st.metric(label="Total deliveries",value=f"{des_kpi_total:,.0f}")
-    #     st.caption("Trip UUIDs count")
+    with kpi_1:
+        st.metric(label="Total deliveries",value=f"{des_kpi_total:,.0f}")
+        st.caption("Trip UUIDs count")
 
-    # with kpi_2:
-    #     st.metric(label="ATD Median (Min)",value=des_kpi_atd)
-    #     st.caption("ATD Median (Min)")
+    with kpi_2:
+        st.metric(label="ATD Median (Min)",value=des_kpi_atd)
+        st.caption("ATD Median (Min)")
 
-    # with kpi_3:
-    #     st.metric(label="Velocity median (Km/Min)",value=des_kpi_velocity)
-    #     st.caption("Total distance (Km) / ATD (Min) median")
+    with kpi_3:
+        st.metric(label="Velocity median (Km/Min)",value=des_kpi_velocity)
+        st.caption("Total distance (Km) / ATD (Min) median")
     
-    # # Graphs and visuals display
-    # graph_1, graph_2, graph_3 = st.columns([2, 2, 2])
+    # Graphs and visuals display
+    graph_1, graph_2, graph_3 = st.columns([2, 2, 2])
     
-    # with graph_1:
-    #     st.markdown("ATD vs Total distance by archetype")
-    #     graph=des_visuals_archetype_scatterplot(df[['geo_archetype','delivery_trip_uuid','pickup_distance','dropoff_distance','ATD']])
-    #     st.pyplot(graph)
+    with graph_1:
+        st.markdown("ATD vs Total distance by archetype")
+        graph=des_visuals_archetype_scatterplot(df[['geo_archetype','delivery_trip_uuid','pickup_distance','dropoff_distance','ATD']])
+        st.pyplot(graph)
 
-    # with graph_2:
-    #     st.markdown("ATD vs Total distance by territory")
-    #     graph=des_visuals_territory_scatterplot(df[['territory','delivery_trip_uuid','pickup_distance','dropoff_distance','ATD']])
-    #     st.pyplot(graph)
+    with graph_2:
+        st.markdown("ATD vs Total distance by territory")
+        graph=des_visuals_territory_scatterplot(df[['territory','delivery_trip_uuid','pickup_distance','dropoff_distance','ATD']])
+        st.pyplot(graph)
     
-    # with graph_3:
-    #     st.markdown("Archetype total deliveries by territory")
-    #     graph=des_visuals_stacked_bar(df[['territory','geo_archetype']])
-    #     st.pyplot(graph)
+    with graph_3:
+        st.markdown("Archetype total deliveries by territory")
+        graph=des_visuals_stacked_bar(df[['territory','geo_archetype']])
+        st.pyplot(graph)
     
-    # graph_4, graph_5= st.columns([1, 1])
-    # with graph_4:
-    #     st.markdown("Weekday analysis")
-    #     graph=des_visuals_day_bars(df[['weekday','ATD', 'driver_uuid']])
-    #     st.pyplot(graph)
+    graph_4, graph_5= st.columns([1, 1])
+    with graph_4:
+        st.markdown("Weekday analysis")
+        graph=des_visuals_day_bars(df[['weekday','ATD', 'driver_uuid']])
+        st.pyplot(graph)
 
-    # with graph_5:
-    #     st.markdown("Summary table")
-    #     summarized_df=des_visuals_table(df[['delivery_trip_uuid','geo_archetype','territory','courier_flow','ATD','pickup_distance','dropoff_distance']])
-    #     AgGrid(summarized_df, height=620)
+    with graph_5:
+        st.markdown("Summary table")
+        summarized_df=des_visuals_table(df[['delivery_trip_uuid','geo_archetype','territory','courier_flow','ATD','pickup_distance','dropoff_distance']])
+        AgGrid(summarized_df, height=620)
     
-    # graph_6, graph_7 = st.columns([4, 1])
+    graph_6, graph_7 = st.columns([4, 1])
     
-    # with graph_6:
-    #     st.markdown("Hour analysis")
-    #     graph=des_visuals_hour_bars(df[['hour_of_day','ATD']])
-    #     st.pyplot(graph)
+    with graph_6:
+        st.markdown("Hour analysis")
+        graph=des_visuals_hour_bars(df[['hour_of_day','ATD']])
+        st.pyplot(graph)
     
-    # with graph_7:
-    #     st.markdown("Deliveries by courier flow")
-    #     graph=des_visuals_pie(df[['courier_flow']])
-    #     st.pyplot(graph)
+    with graph_7:
+        st.markdown("Deliveries by courier flow")
+        graph=des_visuals_pie(df[['courier_flow']])
+        st.pyplot(graph)
 
 if page=='ATD prediction':
     st.subheader("ATD prediction")
@@ -183,8 +183,8 @@ if page=='ATD prediction':
     var_merchant=st.sidebar.multiselect('Merchant Surface',['Other', 'POS', 'Tablet', 'Unspecified', 'Web/Mobile'])
     var_weekday=st.sidebar.slider(label='Weekday (0-Monday, ... ,6-Sunday)', min_value=0, max_value=6)
     var_hour=st.sidebar.slider(label='Hour', min_value=0, max_value=23)
-    var_pickup_distance=st.sidebar.slider(label='Pickup distance', min_value=0,  max_value=40)
-    var_dropoff_distance=st.sidebar.slider(label='Dropoff distance', min_value=0, max_value=120)
+    var_pickup_distance=st.sidebar.slider(label='Pickup distance', min_value=0,  max_value=10)
+    var_dropoff_distance=st.sidebar.slider(label='Dropoff distance', min_value=0, max_value=20)
     
     if st.button('Predict'):
     # Input data
